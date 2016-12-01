a9ab3614e2cd694ca7273588b2e856b7db02f3b3
Christoph Lange <christoph.lange@daimler.com>
2016-12-01 12:52:41 +0100

    Add properties

--- src/main/java/de/sulzer/stgt/sample/springboot/api/SampleController.java
+++ src/main/java/de/sulzer/stgt/sample/springboot/api/SampleController.java
@@ -1,6 +1,9 @@
 package de.sulzer.stgt.sample.springboot.api;
 
 
+import de.sulzer.stgt.sample.springboot.config.SampleProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,12 +11,20 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableConfigurationProperties(SampleProperties.class)
 public class SampleController {
+
+    private SampleProperties sampleProperties;
+
+    @Autowired
+    public SampleController(SampleProperties sampleProperties) {
+        this.sampleProperties = sampleProperties;
+    }
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String helloWorld() {
-        return "Hello World";
+        return "Hello "+ sampleProperties.getName();
     }
 
 }
