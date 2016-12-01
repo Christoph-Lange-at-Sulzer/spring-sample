696e101c9e3ad5a6b05336f548f4fcc8a81fa0ae
Christoph Lange <christoph.lange@daimler.com>
2016-12-01 12:31:35 +0100

    Empty project setup

--- src/main/java/de/sulzer/stgt/sample/springboot/exception/PersonNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package de.sulzer.stgt.sample.springboot.exception;
-
-
-public class PersonNotFoundException extends Exception {
-    public PersonNotFoundException(String msg) {
-        super(msg);
-    }
-}
