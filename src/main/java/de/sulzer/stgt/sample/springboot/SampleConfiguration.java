da2453a0e9c5616a12a619655abf6d0af5e3de0f
Stefan Dupke <stefan.dupke@daimler.com>
2016-12-02 10:27:32 +0100

    STEP_A: Added new ApiInfo.

--- src/main/java/de/sulzer/stgt/sample/springboot/SampleConfiguration.java
+++ src/main/java/de/sulzer/stgt/sample/springboot/SampleConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +19,7 @@ public class SampleConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfo("Sulzer Sample API", "", "0.0.1-ALPHA", "", "stuttgart@sulzer.de", "", ""))
+                .apiInfo(new ApiInfo("Sulzer Sample API", "", "0.0.1-ALPHA", "", new Contact("Sulzer GmbH", "https://www.sulzer.de/", "stuttgart@sulzer.de"), "", ""))
                 .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("de.sulzer.stgt.sample.springboot.api"))
